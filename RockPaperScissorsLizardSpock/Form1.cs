cfce8e24624c3a8e686bb4d61e9e567371adabd6
KevinOSull <kosullivan3336@gmail.com>
2026-08-25 12:51:46 +0100

    Added const variable

--- RockPaperScissorsLizardSpock/Form1.cs
+++ RockPaperScissorsLizardSpock/Form1.cs
@@ -2,6 +2,8 @@ namespace RockPaperScissorsLizardSpock
 {
     public partial class Form1 : Form
     {
+
+        private const int BEST_OF_THREE_GAMES = 3;
         public Form1()
         {
             InitializeComponent();
