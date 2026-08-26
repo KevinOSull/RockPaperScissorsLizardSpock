cceaa64d849510f2cf2f703b54359beefe179d89
KevinOSull <kosullivan3336@gmail.com>
2026-08-26 02:20:22 +0100

    Added enum class for gamestatus and reference in forms class

--- RockPaperScissorsLizardSpock/Form1.cs
+++ RockPaperScissorsLizardSpock/Form1.cs
@@ -7,6 +7,7 @@ namespace RockPaperScissorsLizardSpock
     public partial class Form1 : Form
     {
 
+        private GameStatus gameStatus;
         private const int BEST_OF_THREE_GAMES = 3;
         public Form1()
         {
---
+++ RockPaperScissorsLizardSpock/GameStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissorsLizardSpock
+{
+    public enum GameStatus
+    {
+        GAME_IN_PROGRESS,GAME_OVER
+    }
+}
