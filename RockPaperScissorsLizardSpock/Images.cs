0cbc4d051e4c4f6954b3031dba4a0ac3638b7198
KevinOSull <kosullivan3336@gmail.com>
2026-08-27 23:15:29 +0100

    Set up images class and array to hold gameplay images

---
+++ RockPaperScissorsLizardSpock/Images.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissorsLizardSpock
+{
+    public class Images
+    {
+        public static Image[] gameImages =
+        {
+            Image.FromFile("Resources/rock.jpg"),
+            Image.FromFile("Resources/paper.jpg"),
+            Image.FromFile("Resources/scissors.jpg"),
+            Image.FromFile("Resources/lizard.png"),
+            Image.FromFile("Resources/lizard.png")
+        };
+    }
+}
